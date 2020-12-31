fd9c45beb78ec914d1263e123431fb5dcf5b75c1
Ryan Towner <townerr@yahoo.com>
2020-12-31 16:16:11 -0600

    Initial mongo tests

---
+++ FakebookNotifications/FakebookNotifications.Testing/MongoDbTests.cs
@@ -0,0 +1,56 @@
+﻿using FakebookNotifications.DataAccess;
+using FakebookNotifications.DataAccess.Models;
+using FakebookNotifications.DataAccess.Models.Interfaces;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using Xunit;
+
+namespace FakebookNotifications.Testing
+{
+    public class MongoDbTests
+    {
+        //Test if connection to database is successfull
+        [Fact]
+        public void MongoDbConnectionTest()
+        {
+            //Arrange
+            var mockSettings = Mock.Of<IOptions<NotificationsDatabaseSettings>>();
+            var mockContext = new Mock<NotificationsContext>(mockSettings.Value);
+
+            //Act
+            var result = mockContext.Object.Connect();
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MongoUserCollectionTest()
+        {
+            //Arrange
+            var mockSettings = Mock.Of<IOptions<NotificationsDatabaseSettings>>();
+            var mockContext = new Mock<NotificationsContext>(mockSettings.Value);
+
+            //Act
+            var result = mockContext.Object.User;
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void MongoNotificationCollectionTest()
+        {
+            //Arrange
+            var mockSettings = Mock.Of<IOptions<NotificationsDatabaseSettings>>();
+            var mockContext = new Mock<NotificationsContext>(mockSettings.Value);
+
+            //Act
+            var result = mockContext.Object.Notifications;
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
