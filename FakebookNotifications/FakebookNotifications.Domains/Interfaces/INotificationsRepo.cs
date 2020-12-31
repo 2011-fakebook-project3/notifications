4e6796181c8874aca5e0b2b573a61149630b7f87
Jordan <jordaneddygarcia@gmail.com>
2020-12-31 08:50:09 -0600

    [Enhancement] Added Skeleton for FakebookNotifications
    * Added Interfaces
    * Added Domain Class for User and Notifications

---
+++ FakebookNotifications/FakebookNotifications.Domains/Interfaces/INotificationsRepo.cs
@@ -0,0 +1,19 @@
+﻿using FakebookNotifications.Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakebookNotifications.DataAccess.Models.Interfaces
+{
+    public interface INotificationsRepo
+    {
+        // return a enumarble notifications
+        IEnumerable<Notification> GetAllNotifications();
+        // add a notification to the databse
+        Task<bool> AddNotification();
+        // remove a notification to the database
+        Task<bool> DeleteNotification();
+    }
+}
---
+++ FakebookNotifications/FakebookNotifications.Domains/Interfaces/IUserRepo.cs
@@ -0,0 +1,28 @@
+﻿using FakebookNotifications.Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakebookNotifications.DataAccess.Models.Interfaces
+{
+    interface IUserRepo
+    {
+        /// <summary>
+        /// Gets a User
+        /// </summary>
+        /// <returns> A user with it's email and Id</returns>
+        Task<User> GetUser();
+        /// <summary>
+        /// Creates a User
+        /// </summary>
+        /// <returns>A bool that let's us know if the project passed or fail</returns>
+        Task<bool> CreateUser();
+        /// <summary>
+        /// Delete a User
+        /// </summary>
+        /// <returns> A bool that lets us know if it passed or failed </returns>
+        Task<bool> DeleteUser();
+    }
+}
