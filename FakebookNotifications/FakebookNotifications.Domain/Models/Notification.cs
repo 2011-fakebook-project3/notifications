2febb464db37ae1691a21467666e5cf2e65f2169
Jordan <jordaneddygarcia@gmail.com>
2020-12-31 15:44:24 -0600

    [Feature] Created Domain Skeleton
    * Created Interface for Repositories
    * Created Domain Models
    * Added Documentation for Interfaces

---
+++ FakebookNotifications/FakebookNotifications.Domain/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FakebookNotifications.Domain.Models
+{
+    public class Notification
+    {
+        public string Id { get; set; }
+        public KeyValuePair<string, int> Type { get; set; }
+        public string LoggedInUserId { get; set; }
+        public string TriggerUserId { get; set; }
+        public bool HasBeenRead { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ FakebookNotifications/FakebookNotifications.Domain/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FakebookNotifications.Domain.Models
+{
+    public class User
+    {
+        private string _id;
+        /// <summary>
+        /// Assigns _id an Id number, after verifying it.
+        /// </summary>
+        public string Id {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                // checks to see if int is parsable if not we assign it to a value of 0;
+                if (Int32.TryParse(value, out _))
+                {
+                    _id = value;
+                }
+                else
+                {
+                    Console.WriteLine($"Int32.TryParse could not parse '{value}' to an int.");
+                    _id = "";
+                }
+            }
+                
+                }
+        public string Email { get; set; }
+
+        public User(string id, string email)
+        {
+            Id = id;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Checks whether a user is a valid object or not.
+        /// </summary>
+        /// <returns>
+        /// A boolean.
+        /// </returns>
+        public bool IsValid()
+        {
+            return !(string.IsNullOrEmpty(Id) || string.IsNullOrEmpty(Email));
+        }
+    }
+}
