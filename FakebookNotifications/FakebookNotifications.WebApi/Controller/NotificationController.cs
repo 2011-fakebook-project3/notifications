0891798b8c6d087ebb15ea3bcda1ac29f962b730
Josh Weaver <jweaver86@aim.com>
2021-04-05 13:24:16 -0400

    added code to make tests pass

--- FakebookNotifications/FakebookNotifications.WebApi/Controller/NotificationController.cs
+++ FakebookNotifications/FakebookNotifications.WebApi/Controller/NotificationController.cs
@@ -2,6 +2,9 @@
 using System;
 using FakebookNotifications.WebApi.Hubs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using FakebookNotifications.Domain.Interfaces;
+using FakebookNotifications.Domain.Models;
 
 namespace FakebookNotification.WebApi.Controllers
 {
@@ -10,9 +13,16 @@ namespace FakebookNotification.WebApi.Controllers
 
     public class NotificationController : ControllerBase
     {
-        private readonly NotificationHub hub;
+        private readonly IHubContext<NotificationHub> hub;
+        private readonly INotificationsRepo notificationsRepo;
+        private readonly IUserRepo userRepo;
 
-        public NotificationController(NotificationHub _hub) => hub = _hub;
+        public NotificationController(IHubContext<NotificationHub> _hub, INotificationsRepo _notificationsRepo, IUserRepo _userRepo) 
+        { 
+            hub = _hub;
+            notificationsRepo = _notificationsRepo;
+            userRepo = _userRepo;
+        }
 
         [HttpPost("comment")]
         public async System.Threading.Tasks.Task<IActionResult> CommentNotificationAsync(string loggedInUser, string triggerUser, int postId)
@@ -24,19 +34,26 @@ namespace FakebookNotification.WebApi.Controllers
                 TriggerUserId = triggerUser,
                 Date = DateTime.Now
             };
-            
+
             try
             {
-                await hub.CreateNotification(notification);
-                return Ok();
+                await notificationsRepo.CreateNotificationAsync(notification);
+                User user = await userRepo.GetUserAsync(triggerUser);
+
+                foreach (var connection in user.Connections)
+                {
+                    await hub.Groups.AddToGroupAsync(connection, user.Email);
+                }
+                
+                await hub.Clients.Group(user.Email).SendAsync("CommentNotification", notification);
             }
-            catch(Exception e) 
+            catch (Exception e)
             {
-                //do something better
                 Console.WriteLine(e);
-                return StatusCode(500);
+                throw new NullReferenceException("User not connected");
             }
 
+            return Ok();
         }
 
         [HttpPost("like")]
@@ -44,24 +61,30 @@ namespace FakebookNotification.WebApi.Controllers
         {
             FakebookNotifications.Domain.Models.Notification notification = new FakebookNotifications.Domain.Models.Notification
             {
-                Type = new System.Collections.Generic.KeyValuePair<string, int>("like", postId),
+                Type = new System.Collections.Generic.KeyValuePair<string, int>("post", postId),
                 LoggedInUserId = loggedInUser,
                 TriggerUserId = triggerUser,
                 Date = DateTime.Now
             };
-            
+
             try
             {
-                await hub.CreateNotification(notification);
-                return Ok();
-            }
-            catch(Exception e) 
-            {
-                //do something better
-                Console.WriteLine(e);
-                return StatusCode(500);
-            }
+                await notificationsRepo.CreateNotificationAsync(notification);
+                User user = await userRepo.GetUserAsync(triggerUser);
 
+                foreach (var connection in user.Connections)
+                {
+                    await hub.Groups.AddToGroupAsync(connection, user.Email);
+                }
+                
+                await hub.Clients.Group(user.Email).SendAsync("LikeNotification", notification);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new NullReferenceException("User not connected");
+            }
+            return Ok();
         }
 
         [HttpPost("follow")]
@@ -69,24 +92,31 @@ namespace FakebookNotification.WebApi.Controllers
         {
             FakebookNotifications.Domain.Models.Notification notification = new FakebookNotifications.Domain.Models.Notification
             {
-                Type = new System.Collections.Generic.KeyValuePair<string, int>("follow", postId),
+                Type = new System.Collections.Generic.KeyValuePair<string, int>("follow", profileId),
                 LoggedInUserId = loggedInUser,
                 TriggerUserId = triggerUser,
                 Date = DateTime.Now
             };
-            
+
             try
             {
-                await hub.CreateNotification(notification);
-                return Ok();
+                await notificationsRepo.CreateNotificationAsync(notification);
+                User user = await userRepo.GetUserAsync(triggerUser);
+
+                foreach (var connection in user.Connections)
+                {
+                    await hub.Groups.AddToGroupAsync(connection, user.Email);
+                }
+                
+                await hub.Clients.Group(user.Email).SendAsync("FollowNotification", notification);
             }
-            catch(Exception e) 
+            catch (Exception e)
             {
-                //do something better
                 Console.WriteLine(e);
-                return StatusCode(500);
+                throw new NullReferenceException("User not connected");
             }
 
+            return Ok();
         }
     }
 }
