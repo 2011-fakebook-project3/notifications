5cebe2ab17c59d0976a3d01198899686287fa995
John Werner <jwerner547@gmail.com>
2021-04-19 09:42:02 -0600

    adding the missing variables for _env and options

--- FakebookNotifications/FakebookNotifications.WebApi/Startup.cs
+++ FakebookNotifications/FakebookNotifications.WebApi/Startup.cs
@@ -17,12 +17,14 @@ namespace FakebookNotifications.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -59,11 +61,11 @@ namespace FakebookNotifications.WebApi
             {
                 if (_env.IsDevelopment())
                 {
-                    o.Authority = "https://localhost:44374";
+                    options.Authority = "https://localhost:44374";
                 }
                 else
                 {
-                    o.Authority = "https://fakebook.revaturelabs.com";
+                    options.Authority = "https://fakebook.revaturelabs.com";
                 }
                 options.Audience = "fakebookApi";
                 options.Events = new JwtBearerEvents
